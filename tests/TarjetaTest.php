29c9c8e0ee2677e4b74eb6b882969666c06a69db
agustindelmonti <agus.delmonti@gmail.com>
2016-09-26 23:12:27 -0300

    Test prueba

---
+++ tests/TarjetaTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Poli\Tarjeta;
+
+class TarjetaTest extends \PHPUnit_Framework_TestCase {
+
+	protected $tarjeta,$A,$B;
+	public function setup(){
+		$this->tarjeta = new Tarjeta();
+		$this->A = new Colectivo("K","SEMTUR");
+		$this->B = new Colectivo("145","Rosario Bus")
+
+	}
+	public function recargarTest1(){
+		$this->tarjeta->recargar(600)
+		$this->assertEquals($this->tarjeta->saldo(), 740, "Cuando cargo 600 deberia tener finalmente 740");
+	}
+	public function recargarTest2(){
+		$this->tarjeta->recargar(350)
+		$this->assertEquals($this->tarjeta->saldo(), 398, "Cuando cargo 350 deberia tener finalmente 398");
+	}
+	public function recargarTest3(){
+		$this->tarjeta->recargar(250)
+		$this->assertEquals($this->tarjeta->saldo(), 250, "Cuando cargo 250 deberia tener finalmente 250");
+	}
+
+}
+?>
