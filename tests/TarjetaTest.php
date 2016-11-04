7226da06acb8a6fa01015107667704ba43c18e2c
agustindelmonti <agus.delmonti@gmail.com>
2016-11-03 22:21:25 -0300

    Comentado de codigo/Generalizacion variables

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -32,7 +32,7 @@ class TarjetaTest extends \PHPUnit_Framework_TestCase {
 		$this->tarjeta->recargar(20);
 		$this->tarjeta->pagar($this->B,"2016/02/1 12:00");
 		//Pruebo todas las funciones de la case viajes
-		$this->assertEquals($this->tarjeta->viajesRealizados()["2016/02/1 12:00"]->getCosto(),$boleto_colectivo,"El valor del boleto de colectivo es $".$boleto_colectivo);
+		$this->assertEquals($this->tarjeta->viajesRealizados()["2016/02/1 12:00"]->getCosto(),$this->boleto_colectivo,"El valor del boleto de colectivo es $".$boleto_colectivo);
 		$this->assertEquals($this->tarjeta->viajesRealizados()["2016/02/1 12:00"]->getHorario(),"2016/02/1 12:00"," ");
 		$this->assertEquals($this->tarjeta->viajesRealizados()["2016/02/1 12:00"]->getTransporte()->getId(),"145"," ");
 		$this->assertEquals($this->tarjeta->viajesRealizados()["2016/02/1 12:00"]->getTipo(),"Viaje en Colectivo","Es un colectivo");
