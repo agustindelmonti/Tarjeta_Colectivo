1ed579b541ce522b195fc319b8b032be2894de50
agustindelmonti <agus.delmonti@gmail.com>
2016-11-04 10:09:08 -0300

    Comentado de codigo

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -20,30 +20,34 @@ class Tarjeta implements Int_Tarjeta{
 			$aux1 = strtotime($fecha_y_hora);
 			$aux2 = strtotime($this->ultimafecha);
 
-			//Determino $costo segun transbordo o normal
+			//Calculo costo boleto segun tiempo transcurrido (transbordo/normal)
 			if($this->ultimafecha == 0 || ($aux1-$aux2>3600) || $this->viajes[$this->ultimafecha]->getTransporte()->getId() == $transporte->getid()){ 
 				$costo = $transporte->getCosto()*$this->porcentaje;
 			} else {
 				$costo = $transporte->getCostoTrans()*$this->porcentaje;
 			}
+			//Puede pagar el boleto y los plus, se pagan los plus
 			if($costo+$this->valorPlus <= $this->saldo && $this->plus>0){
 				$this->saldo -= $this->valorPlus;
 				$this->plus = 0;
 				$this->valorPlus = 0;
 			}
+			//No tengo saldo pero tengo plus, se computa el plus
 			if($costo<=$this->saldo || $this->plus<2){
 				if($costo>$this->saldo && $this->plus<2){
 					$this->plus++;
 					$this->valorPlus += $costo;
 				}
+				//Sino se descuenta el boleto del saldo
 				else{
 					$this->saldo -= $costo;
 				}
-
+				//Se crea el viaje, boleto y se puede viajar
 				$this->viajes[$fecha_y_hora] = new Viaje($fecha_y_hora,$transporte,$costo);
 				$this->ultimafecha = $fecha_y_hora;
 				return 1;
 			} 
+			//Si no puedo pagar
 			else{
 				return 0;
 			}
@@ -55,27 +59,36 @@ class Tarjeta implements Int_Tarjeta{
 			$aux1 = strtotime($fecha_y_hora);
 			$aux2 = strtotime($this->ultimabicipaga);
 
+			//Calculo costo boleto segun tiempo transcurrido 
 			if($this->ultimabicipaga == 0 || ($aux1-$aux2>86400)){
 				$costo = $transporte->getCosto();
 			} else {
 				$costo = 0;
 			}
+			//Puede pagar el boleto y los plus, se pagan los plus
 			if(($this->saldo >= $costo+$this->valorPlus)&& $this->plus>0){
 				$this->saldo -= $this->valorPlus;
 				$this->plus = 0;
 				$this->valorPlus = 0;
 			}
+			//Tengo saldo pero tengo plus, se computa el plus
 			if($costo<=$this->saldo || $this->plus<2){
 				if($costo>$this->saldo && $this->plus<2){
 					$this->plus++;
 					$this->valorPlus += $costo;
 				}
+				//Sino se descuenta el boleto del saldo
 				else{
 					$this->saldo -= $costo;
 					$this->ultimabicipaga = $fecha_y_hora;
 				}
-			$this->viajes[$fecha_y_hora] = new Viaje($fecha_y_hora,$transporte,$costo);
-			return 1;
+				//Se crea el viaje, boleto y se puede viajar
+				$this->viajes[$fecha_y_hora] = new Viaje($fecha_y_hora,$transporte,$costo);
+				return 1;
+			}
+			else{
+				//Si no puedo pagar
+				return 0;
 			}
 		}
 	}
