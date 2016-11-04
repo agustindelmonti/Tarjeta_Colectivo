5f6a86e35f1e2d9f51cf67f76742af097ce476b0
agustindelmonti <agus.delmonti@gmail.com>
2016-11-03 22:27:15 -0300

    Generalizacion de variables

--- tests/biciTest.php
+++ tests/biciTest.php
@@ -4,6 +4,8 @@
 
 	class biciTest extends \PHPUnit_Framework_TestCase {
 
+		//Cada vez que aumente el boleto se cambia este parametro para cambiar los tests
+		protected $boleto_bicicleta = 12; 
 		protected $tarjeta,$A,$B,$C;
 
 		public function setup(){
@@ -16,16 +18,16 @@
 		public function testPagarBici(){
 			$this->tarjeta->recargar(30);
 			$this->tarjeta->pagar($this->C,"2016/02/1 12:00");
-			$this->assertEquals($this->tarjeta->saldo(),18, "Saldo deberia ser $18");	
+			$this->assertEquals($this->tarjeta->saldo(),30-$this->boleto_bicicleta, "Saldo deberia ser $".(30-$this->boleto_bicicleta));	
 		}
 
 		public function testPagarDia(){
 			$this->tarjeta->recargar(30);
 			$this->tarjeta->pagar($this->C,"2016/02/1 12:00");
 			$this->tarjeta->pagar($this->C,"2016/02/1 15:00"); //Bicicleta mismo dia ($0)
-			$this->assertEquals($this->tarjeta->saldo(),18, "Saldo deberia ser $18");
-			$this->tarjeta->pagar($this->C,"2016/02/3 12:00"); //Bicicleta otro dia ($0)
-			$this->assertEquals($this->tarjeta->saldo(),6, "Saldo deberia ser $6");
+			$this->assertEquals($this->tarjeta->saldo(),30-$this->boleto_bicicleta, "Saldo deberia ser $".(30-$this->boleto_bicicleta));
+			$this->tarjeta->pagar($this->C,"2016/02/3 12:00"); //Bicicleta otro dia ($12)
+			$this->assertEquals($this->tarjeta->saldo(),30-2*$this->boleto_bicicleta, "Saldo deberia ser $".(30-2*$this->boleto_bicicleta));
 		}
 		public function testPagarPlus(){
 			$this->tarjeta->recargar(2.63);
@@ -45,7 +47,7 @@
 			$this->tarjeta->pagar($this->C,"2016/04/1 12:00"); //plus 2
 			$this->tarjeta->recargar(40); //Recargo de nuevo
 			$this->tarjeta->pagar($this->C,"2016/06/1 12:00");
-			$this->assertEquals($this->tarjeta->saldo(),6.63,"Saldo deberia ser $6.63"); //Pago plus($24) y un boleto($12)
+			$this->assertEquals($this->tarjeta->saldo(),42.63-3*$this->boleto_bicicleta,"Saldo deberia ser $".(42.63-3*$this->boleto_bicicleta)); //Pago plus($24) y un boleto($12)
 		}
 	}
 ?>
